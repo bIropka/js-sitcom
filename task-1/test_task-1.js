4e5f85261efa13cfeb088e6c066ee8df9735d205
Iegor Stetsiura <biropka@gmail.com>
2019-07-15 17:12:23 +0300

    equal replaced by strictEqual

--- task-1/test_task-1.js
+++ task-1/test_task-1.js
@@ -1,34 +1,34 @@
 mocha.setup('bdd');
-var assert = chai.assert;
+const assert = chai.assert;
 
 describe("getChars", function() {
 
     it("выводит false если введена строка", function() {
-        assert.equal(getChars('3'), false);
+        assert.strictEqual(getChars('3'), false);
     });
 
     it("выводит false если введено отрицательное число", function() {
-        assert.equal(getChars(-5), false);
+        assert.strictEqual(getChars(-5), false);
     });
 
     it("выводит false если введено не целое число", function() {
-        assert.equal(getChars(2.7), false);
+        assert.strictEqual(getChars(2.7), false);
     });
 
     it("выводит 'x' если функция вызвана без параметров", function() {
-        assert.equal(getChars(), 'x');
+        assert.strictEqual(getChars(), 'x');
     });
 
     it("выводит 'x' если введено 1", function() {
-        assert.equal(getChars(1), 'x');
+        assert.strictEqual(getChars(1), 'x');
     });
 
     it("выводит 'xxx' если введено 3", function() {
-        assert.equal(getChars(3), 'xxx');
+        assert.strictEqual(getChars(3), 'xxx');
     });
 
     it("выводит 'xxxxxxx' если введено 7", function() {
-        assert.equal(getChars(7), 'xxxxxxx');
+        assert.strictEqual(getChars(7), 'xxxxxxx');
     });
 
 });
