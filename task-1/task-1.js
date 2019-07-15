f856fc790ecb8459add1106437cbcd1d4f411812
Iegor Stetsiura <biropka@gmail.com>
2019-07-15 17:04:15 +0300

    tasl 1 has done

--- task-1/task-1.js
+++ task-1/task-1.js
@@ -1,3 +1,21 @@
-function getChars(n) {
+function getChars_ES5(n) {
+
+    if (typeof n === 'undefined') {
+        return 'x';
+    }
+
+    if (typeof n === 'number') {
+
+        if (n > 0 && (n ^ 0) === n) {
+            return new Array(n).fill('x').join('');
+        }
+
+    }
+
+    return false;
 
 }
+
+const getChars = (n = 1) => typeof n === 'number' && n > 0 && (n ^ 0) === n ? 'x'.repeat(n) : false;
+
+console.log(getChars());
