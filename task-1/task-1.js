365ea2b2f13d57102a41aec6ab1cb4df31188c4f
Inna_B <bondarenkoinnaa@gmail.com>
2019-07-17 11:13:48 +0300

    Add function getChars()

--- task-1/task-1.js
+++ task-1/task-1.js
@@ -1,7 +1,18 @@
-function getChars( n ) {
+function getChars(numbr) {
+	var str = 'x';
 
+	if(typeof numbr === 'string' || numbr < 0) {
+		return false;
+	}else if(numbr === undefined) {
+		return str;
+	}else if(Number(numbr) === numbr && numbr % 1 !== 0) {
+		return false;
+	}else {
+		return str.repeat(numbr);
+	}
 }
 
+/*
 console.log( 'без параметров', getChars() );
 console.log( 'передана строка', getChars('3' ) );
 console.log( 'передана дробь', getChars(2.7 ) );
@@ -9,3 +20,4 @@ console.log( 'передано отрицательное число', getChars(
 console.log( 'передано 1', getChars(1 ) );
 console.log( 'передано 3', getChars(3 ) );
 console.log( 'передано 7', getChars(7 ) );
+*/
