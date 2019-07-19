2599ac243ee1ab0548ab4ac9df2dea6cbd8dff8e
Per1meter <perimeter1989@gmail.com>
2019-07-19 17:05:45 +0300

    task-2 resolved

--- task-2/task-2.js
+++ task-2/task-2.js
@@ -1,5 +1,7 @@
 function whenNY() {
-
+    let nyTime = new Date((new Date().getFullYear()+1), 0, 1, 0, 0, 0).getTime(),
+        nowTime = new Date().getTime();
+    return Math.round((nyTime - nowTime)/(1000*60*60*24));
 }
 
 console.log(whenNY());
