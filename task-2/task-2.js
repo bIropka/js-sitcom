8bbf2fcd67feb8bfd11622513cc8789e2304458b
Inna_B <bondarenkoinnaa@gmail.com>
2019-07-25 12:15:04 +0300

    Add function whenNY()

--- task-2/task-2.js
+++ task-2/task-2.js
@@ -1,5 +1,8 @@
 function whenNY() {
+	var today = new Date(),
+		newYearDate = new Date(2020, 0, 1);
 
+	return Math.trunc((newYearDate - today) / 86400000); //24*60*60*1000ms
 }
 
 console.log(whenNY());
